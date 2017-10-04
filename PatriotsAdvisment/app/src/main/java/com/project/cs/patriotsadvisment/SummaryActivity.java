749a31bc16679e261b2c928f92963fb53d9930fa
nmelone <n_melone@hotmail.com>
2017-10-04 13:05:52 -0500

    Added Circle progress bar
    Added dependencies to gradle to add capability to have a circluar progression bar

--- PatriotsAdvisment/app/src/main/java/com/project/cs/patriotsadvisment/SummaryActivity.java
+++ PatriotsAdvisment/app/src/main/java/com/project/cs/patriotsadvisment/SummaryActivity.java
@@ -1,12 +1,15 @@
 package com.project.cs.patriotsadvisment;
 
 import android.content.Intent;
+import android.icu.text.DecimalFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.github.lzyzsd.circleprogress.DonutProgress;
 
 public class SummaryActivity extends AppCompatActivity {
 
@@ -15,11 +18,13 @@ public class SummaryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
         //creating progress bars
+        DonutProgress circle = (DonutProgress) findViewById(R.id.donut_progress);
         ProgressBar coreProgress = (ProgressBar)findViewById(R.id.coreProgressBar);
         ProgressBar degreeProgress = (ProgressBar)findViewById(R.id.degreeProgressBar);
         ProgressBar overallProgress = (ProgressBar)findViewById(R.id.overallProgressBar);
         //setting progress
         //TODO use data from Database
+        circle.setProgress((int)(Math.random()*100));
         coreProgress.setProgress((int)(Math.random()*100));
         degreeProgress.setProgress((int)(Math.random()*100));
         overallProgress.setProgress((int)(Math.random()*100));
@@ -33,6 +38,12 @@ public class SummaryActivity extends AppCompatActivity {
         overallPercent.setText(overallProgress.getProgress() + "%");
         //creating student label
         TextView student = (TextView)findViewById(R.id.student);
+        TextView gpa = (TextView)findViewById(R.id.gpaText);
+        double gpaNumber = Math.random()*4;
+        gpaNumber = Math.round(gpaNumber*100.00);
+        gpaNumber = gpaNumber/100;
+        Double gpaText = gpaNumber;
+        gpa.setText(gpaText.toString());
         //setting student label
         //TODO use data from Database
         student.setText("John Smith");
