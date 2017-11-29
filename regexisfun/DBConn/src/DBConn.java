3923bfd567af82b8e489d85c8e44d8685bb38798
jwilcher <31670904+jwilcher@users.noreply.github.com>
2017-11-29 14:31:42 -0600

    Add files via upload

---
+++ regexisfun/DBConn/src/DBConn.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class DBConn{
+	static String[] theArgs = new String[3];
+	public static void main(String[] args) {
+/*		Scanner cin = new Scanner(System.in);
+		System.out.println("Please state your purpose (login,query");
+		theArgs[0] = cin.nextLine();
+		System.out.println("Please enter a query statement:");
+		theArgs[1] = cin.nextLine();*/
+		StudentBiz();
+		SpaceBetween();
+		ClassBiz();
+		SpaceBetween();
+		CourseBiz();
+		SpaceBetween();
+		ListTables();
+	}
+	
+	public static void SpaceBetween(){
+		for(int x = 0; x < 20; x++)
+			System.out.print("-");
+		System.out.println("");
+	}
+	
+	private static void StudentBiz(){
+		theArgs[0] = "query";
+		theArgs[1] = "select * from student";
+		theArgs[2] = null;
+		ActConn stuConn = new ActConn(theArgs);
+		ArrayList<String> students = StringParsing.StudentParse(stuConn.makeTheConnection());
+		int q = 1;
+		for(String i : students){
+			System.out.print(i);
+			System.out.print(" , ");
+			if(q%7==0)
+				System.out.println(" ");
+			q++;
+		}
+	}
+	
+	private static void ClassBiz(){
+		theArgs[0] = "query";
+		theArgs[1] = "select * from class";
+		ActConn claConn = new ActConn(theArgs);
+		ArrayList<String> classes = StringParsing.ClassParse(claConn.makeTheConnection());
+		int q = 1;
+		for(String i : classes){
+			if(i.equals("001")==false){
+				System.out.print(i);
+				System.out.print(" , ");
+				if(q%5==0){
+					System.out.println("");
+					q=0;
+				}
+				q++;
+			}
+		}
+		for(String i : classes){
+			System.out.println(i);
+		}
+	}
+	
+	private static void CourseBiz(){
+		theArgs[0] = "query";
+		theArgs[1] = "select * course";
+		ActConn couConn = new ActConn(theArgs);
+		ArrayList<String> courses = StringParsing.CourseParse(couConn.makeTheConnection());
+		int q = 1;
+		for(String i : courses){
+			System.out.println(i);
+		}
+	}
+	
+	private static void ListTables(){
+		theArgs[0] = "query";
+		theArgs[1] = "SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'";
+		ActConn couConn = new ActConn(theArgs);
+		ArrayList<String> courses = StringParsing.TablesParse(couConn.makeTheConnection());
+		int q = 1;
+		for(String i : courses){
+			System.out.println(i);
+		}
+	}
+}
---
+++ regexisfun/DBConn/src/StringParsing.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringParsing {
+    public static ArrayList StudentParse(String inputString){
+        ArrayList<String> matches = new ArrayList();
+        ArrayList<String> OMatches = new ArrayList();
+        Pattern pattern = Pattern.compile("(([\\w\\d\\s&@.:\\/-]+)(,){0,1}([\\w\\d\\s&@.]+))");
+        Pattern pattern2 = Pattern.compile("[{\"][\\ -z]+[\"}]");
+        Matcher matcher = pattern2.matcher(inputString);
+        while(matcher.find()) {
+            OMatches.add(matcher.group());
+        }
+        for(String thisString : OMatches) {
+            matcher = pattern.matcher(thisString);
+            int i = 0;
+            while (matcher.find()) {
+                if(i == 1 || i == 3 || (i > 4 && i%2 == 1))
+                    matches.add(matcher.group());
+                i++;
+            }
+        }
+        return matches;
+    }
+    
+    public static ArrayList ClassParse(String inputString){
+        ArrayList<String> matches = new ArrayList();
+        ArrayList<String> OMatches = new ArrayList();
+        Pattern pattern2 = Pattern.compile("[{\"][\\ -z]+[\"}]");
+        Pattern pattern3 = Pattern.compile("(([\\w\\d\\s&@.:\\/-]+)(,){0,1}([\\w\\d\\s&@.\\/-]+))");
+        Matcher matcher = pattern2.matcher(inputString);
+        while(matcher.find()) {
+            OMatches.add(matcher.group());
+        }
+        for(String thisString : OMatches) {
+            //matcher = pattern.matcher(thisString);
+        	matcher = pattern3.matcher(thisString);
+            int i = 0;
+            int x = 9;
+            while (matcher.find()) {
+                if(i%2!=0) {
+                	if(i == x){
+                		x+=12;
+                		if(matcher.group().charAt(1)=='W')
+                			matches.add("M\\"+matcher.group());
+                		else if(matcher.group().charAt(1)=='T')
+                			matches.add("T\\"+matcher.group());
+                		else
+                			matches.add(matcher.group());
+                	}
+                	else
+                		matches.add(matcher.group());
+                }
+                i++;
+            }
+        }
+        return matches;
+    }
+    
+    public static ArrayList CourseParse(String inputString){
+    	ArrayList<String> matches = new ArrayList();
+        ArrayList<String> OMatches = new ArrayList();
+        Pattern pattern2 = Pattern.compile("[{\"][\\ -z]+[\"}]");
+        Pattern pattern3 = Pattern.compile("[\\w\\s\\d.]+");
+        Matcher matcher = pattern2.matcher(inputString);
+        while(matcher.find()) {
+            OMatches.add(matcher.group());
+        }
+        for(String thisString : OMatches) {
+        	matcher = pattern3.matcher(thisString);
+        	int i = 0;
+        	while (matcher.find()) {
+        		if(i%2!=0) {
+        			matches.add(matcher.group());
+        		}
+        		i++;
+        	}
+        }
+        return matches;
+    }
+    
+    public static ArrayList TablesParse(String inputString){
+    	ArrayList<String> matches = new ArrayList();
+        ArrayList<String> OMatches = new ArrayList();
+        Pattern pattern2 = Pattern.compile("[{\"][\\ -z]+[\"}]");
+        Pattern pattern3 = Pattern.compile("[\\w\\s\\d.]+");
+        Matcher matcher = pattern2.matcher(inputString);
+        while(matcher.find()) {
+            OMatches.add(matcher.group());
+        }
+        /*for(String thisString : OMatches) {
+        	matcher = pattern3.matcher(thisString);
+        	int i = 0;
+        	while (matcher.find()) {
+        		if(i%2!=0) {
+        			matches.add(matcher.group());
+        		}
+        		i++;
+        	}
+        }*/
+        matches = OMatches;
+        return matches;
+    }
+}
