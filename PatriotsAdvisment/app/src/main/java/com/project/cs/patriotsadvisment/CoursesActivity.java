430b0abcbbb28e35cf1e30f81dae911704d73215
nmelone <n_melone@hotmail.com>
2017-12-05 13:00:18 -0600

    Added support for courses to be populated
    Still needs some slight tweaking for regex to display properly.

--- PatriotsAdvisment/app/src/main/java/com/project/cs/patriotsadvisment/CoursesActivity.java
+++ PatriotsAdvisment/app/src/main/java/com/project/cs/patriotsadvisment/CoursesActivity.java
@@ -10,6 +10,9 @@ import android.widget.ExpandableListView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CoursesActivity extends Activity {
 
@@ -17,6 +20,7 @@ public class CoursesActivity extends Activity {
     ExpandableListView coursesEL;
     List<String> listCoursesHeader;
     HashMap<String, List<String>> listCoursesChild;
+    String rawQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +38,7 @@ public class CoursesActivity extends Activity {
         //setting list adapter
         coursesEL.setAdapter(listAdapter);
 
-        //gets all classes for a degree plan that aren't electives
-        //select course_code from course natural join rq_ln_course natural join requirement where degree_name = 'computer information systems' and (rq_code != 'rq 1161' or rq_code != 'rq 1158') order by course_code
+
     }
 
     private void prepareListData(){
@@ -50,33 +53,78 @@ public class CoursesActivity extends Activity {
         listCoursesHeader.add("Senior Courses");
 
 
+        //gets all classes for a degree plan that aren't electives
+        rawQuery = makeQuery("select name from course natural join rq_ln_course natural join requirement where degree_name = 'computer information systems' and (rq_code != 'rq 1161' and rq_code != 'rq 1158') and course_code like '____ 1___' order by course_code;");
+        ArrayList<String> rawFreshmanArray = parseTheData(rawQuery);
+
         //final program will use a for loop to add all the proper data
         //TODO change all the adding to a loop that gets data from database
         List<String> freshman = new ArrayList<String>();
-        freshman.add("English 101");
-        freshman.add("Math 101");
-        freshman.add("History 101");
+        for(int i = 1; i < rawFreshmanArray.size(); i+=2){
+            freshman.add(rawFreshmanArray.get(i));
+        }
 
+        //gets all classes for a degree plan that aren't electives
+        rawQuery = makeQuery("select name from course natural join rq_ln_course natural join requirement where degree_name = 'computer information systems' and (rq_code != 'rq 1161' and rq_code != 'rq 1158') and course_code like '____ 2___' order by course_code;");
+        ArrayList<String> rawSophomoreArray = parseTheData(rawQuery);
         List<String> sophomore = new ArrayList<String>();
-        sophomore.add("English 201");
-        sophomore.add("Math 201");
-        sophomore.add("History 201");
 
+        for(int i = 1; i < rawSophomoreArray.size(); i+=2){
+            sophomore.add(rawSophomoreArray.get(i));
+        }
+
+        //gets all classes for a degree plan that aren't electives
+        rawQuery = makeQuery("select name from course natural join rq_ln_course natural join requirement where degree_name = 'computer information systems' and (rq_code != 'rq 1161' and rq_code != 'rq 1158') and course_code like '____ 3___' order by course_code;");
+        ArrayList<String> rawJuniorArray = parseTheData(rawQuery);
         List<String> junior = new ArrayList<String>();
-        junior.add("Physics 301");
-        junior.add("Calculus 301");
-        junior.add("Long Division 12");
+        for(int i = 1; i < rawJuniorArray.size(); i+=2){
+            junior.add(rawJuniorArray.get(i));
+        }
 
+        rawQuery = makeQuery("select name from course natural join rq_ln_course natural join requirement where degree_name = 'computer information systems' and (rq_code != 'rq 1161' and rq_code != 'rq 1158') and course_code like '____ 4___' order by course_code;");
+        ArrayList<String> rawSeniorArray = parseTheData(rawQuery);
         List<String> senior = new ArrayList<String>();
-        senior.add("Slacking off 101");
-        senior.add("Graduating LOL");
-        senior.add("Fuckitall.exe");
+        for(int i = 1; i < rawSeniorArray.size(); i+=2){
+            senior.add(rawSeniorArray.get(i));
+        }
 
         //putting all the children into the right list
         listCoursesChild.put(listCoursesHeader.get(0), freshman);
         listCoursesChild.put(listCoursesHeader.get(1), sophomore);
         listCoursesChild.put(listCoursesHeader.get(2), junior);
         listCoursesChild.put(listCoursesHeader.get(3), senior);
+    }
+    //used to get data from a raw query return
+    public ArrayList<String> parseTheData(String inputString){
+        ArrayList<String> matches = new ArrayList();
+        ArrayList<String> OMatches = new ArrayList();
+        Pattern pattern = Pattern.compile("(([\\w\\d\\s&@.:\\/-]+)(,){0,1}([\\w\\d\\s&@.]+))");
+        Pattern pattern2 = Pattern.compile("[{\"][\\ -z]+[\"}]");
+        Matcher matcher = pattern2.matcher(inputString);
+        while(matcher.find()) {
+            OMatches.add(matcher.group());
+        }
+        for(String thisString : OMatches) {
+            matcher = pattern.matcher(thisString);
+            while (matcher.find()) {
+                matches.add(matcher.group());
+            }
+        }
+        return matches;
+    }
+    //used to make a query and get a return
+    private String makeQuery(String s){
+        String output = "";
+        try {
+            DBconn myconn = new DBconn(this);
+
+            output = myconn.execute("query",s).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return output;
     }
 
 
